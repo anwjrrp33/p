f4c3dc9eb896bba57480581caf43d71d041109fa
hb03-30 <hb03-30@BIT01-22>
2018-05-02 20:26:07 +0900

    4. 화면에서 /board/list 처리

---
+++ p/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,35 @@
+package org.zerock.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
+import org.zerock.mapper.PageMapper;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@RequestMapping("/board/*")
+@Log4j
+public class BoardController {
+
+	@Setter(onMethod_={@Autowired})	
+	private PageMapper mapper;
+	
+	@GetMapping("/list/{page}")
+	public List<BoardVO> list(Criteria cri) {
+				
+		log.info("cri: " + cri);
+		
+		log.info(mapper.getList(cri));
+			
+		return  mapper.getList(cri);
+		
+	}	
+}
